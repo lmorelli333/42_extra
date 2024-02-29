ce090b70b839aad40586b2727813b4838895238c
lmorelli333 <118579575+lmorelli333@users.noreply.github.com>
2024-02-29 18:23:59 +0100

    Delete Back to the past _Δ directory

--- Back to the past _Δ/scene/Script/Delorean.gd
+++
@@ -1,4 +0,0 @@
-extends AnimatedSprite2D
-
-func _on_animation_finished():
-	get_tree().change_scene_to_file("res://scene/change_level.tscn")
--- Back to the past _Δ/scene/Script/HUD.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-
-func _ready():
-	pass # Replace with function body.
-
-func _process(delta):
-	$value.text = str(Global.player_health)
-	$batt_taken.text = str(Global.energy)
